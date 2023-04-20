42f4bbeb6891d7b3a9931ef250aa864d9d9f09f6
Moor1690 <vehfl9933@gmail.com>
2023-04-20 19:50:58 +0300

    added minimal interface

--- app/src/main/java/com/example/forsamsung012/MainActivity.kt
+++ app/src/main/java/com/example/forsamsung012/MainActivity.kt
@@ -1,46 +1,48 @@
 package com.example.forsamsung012
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import com.example.forsamsung012.navigate.Navigate
 import com.example.forsamsung012.ui.theme.Forsamsung012Theme
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Forsamsung012Theme {
+                val navController: NavHostController = rememberAnimatedNavController()
+                val isShowBottomBar = remember { mutableStateOf(false) }
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()//,
+                    //color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Navigate(
+                        navController = navController,
+                        context = this,
+                        isShowBottomBar = isShowBottomBar
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Forsamsung012Theme {
-        Greeting("Android")
     }
 }
