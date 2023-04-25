d7d89466dfdf39310c05e6479a5cf26259fb7512
Moor1690 <vehfl9933@gmail.com>
2023-04-26 02:01:17 +0300

    added SwipeToDismiss (test)

--- app/src/main/java/com/example/forsamsung012/screens/ListScreen.kt
+++ app/src/main/java/com/example/forsamsung012/screens/ListScreen.kt
@@ -3,6 +3,9 @@ package com.example.forsamsung012.screens
 import android.annotation.SuppressLint
 import android.content.ClipData.Item
 import android.content.Context
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
@@ -21,14 +24,19 @@ import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomAppBar
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material.rememberSwipeableState
 import androidx.compose.material.swipeable
@@ -40,6 +48,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -52,16 +61,63 @@ import com.example.forsamsung012.bar.MyAppBar
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
+
+import androidx.compose.material.SwipeToDismiss
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.rememberDismissState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.draw.scale
+
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "UnusedMaterialScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun ListScreen(navController: NavHostController, context: Context) {
+
+    data class User(var name: String = "", var age: Int = 0){
+        
+    }
+
     val password = remember { mutableStateOf("") }
     //Text(text = "List Screen")
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
     val swipeableState = rememberSwipeableState(initialValue = 0)
+    val person1 = User("John")
+    val person2 = User("Johan2")
+    person1.age = 10
+    person2.age = 20
 
+    var itemsList = mutableListOf<User>()
+    var itemsList2 = mutableListOf<String>()
+    itemsList2.add("asdasd")
+    itemsList2.add("qe")
+    itemsList2.add("asasadsddasd")
+    itemsList2.add("asdqweasd")
+    itemsList2.add("asdasdxazasd")
+    itemsList2.add("asdaasdqg2334sd")
+    itemsList2.add("as12335dasd")
+    itemsList2.add("asdasd")
+    itemsList2.add("qe")
+    itemsList2.add("asasadsddasd")
+    itemsList2.add("asdqweasd")
+    itemsList2.add("asdasdxazasd")
+    itemsList2.add("asdaasdqg2334sd")
+    itemsList2.add("as12335dasd")
+    itemsList.add(person1)
+    itemsList.add(person2)
+    itemsList.add(person1)
+    itemsList.add(person2)
+    itemsList.add(person1)
+    itemsList.add(person2)
+    itemsList.add(person1)
+    itemsList.add(person2)
+    itemsList.add(person1)
+    itemsList.add(person2)
+    itemsList.add(person1)
+    itemsList.add(person2)
+
+    //itemsList2[1].
 
     Scaffold(
         Modifier.background(color = Color.Red),
@@ -84,61 +140,91 @@ fun ListScreen(navController: NavHostController, context: Context) {
             Text(text = "text3")
         }
     ) {
-    LazyColumn{
-        items(15){
-            Box(
-                modifier = Modifier
-                    .clip(shape = RoundedCornerShape(10.dp))
-                    .padding(8.dp)
-                    .background(Color.Red)
-                    .fillMaxWidth()
-                    .heightIn(80.dp)
-                    .swipeable(
-                        state = swipeableState,
-                        anchors = mapOf(
-                            0f to 0,
-                            -dpToPX(context = context, dpValue = 100f) to 1,
-                            dpToPX(context = context, dpValue = 100f) to 2
-                        ),
-                        thresholds = { _, _ -> FractionalThreshold(0.3f) },
-                        orientation = Orientation.Horizontal
-                    )
-            ) {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "btn")
-                }
-                Box(modifier = Modifier
-                    .offset {
-                        IntOffset(swipeableState.offset.value.roundToInt(), 0)
+
+        LazyColumn {
+            items(itemsList) { item ->
+                //val currentItem by rememberUpdatedState(item)
+                val dismissState = rememberDismissState(
+                    confirmStateChange = {
+                        if (it == DismissValue.DismissedToEnd){
+                            //todo
+                        }
+                        else if (it == DismissValue.DismissedToStart){
+                            //todo
+                        }
+                        true
                     }
-                    //.clip(shape = RoundedCornerShape(10.dp))
-                    .fillMaxWidth()
-                    .heightIn(80.dp)
-                    .background(Color.Green)
                 )
+
+                SwipeToDismiss(state = dismissState,
+                    directions = setOf(DismissDirection.StartToEnd,
+                        DismissDirection.EndToStart),
+                        background = {
+                            val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
+                            val color by animateColorAsState(targetValue = when(dismissState.targetValue){
+                                DismissValue.Default -> Color.LightGray
+                                DismissValue.DismissedToEnd -> Color.Green
+                                DismissValue.DismissedToStart -> Color.Red
+                            })
+
+                            val icon = when(direction){
+                                DismissDirection.EndToStart -> Icons.Default.Done
+                                DismissDirection.StartToEnd -> Icons.Default.Delete
+                            }
+
+                            val scale by animateFloatAsState(targetValue = if (dismissState.targetValue == DismissValue.Default) 0.8f else 1.2f)
+
+                            val alignment = when(direction){
+                                DismissDirection.EndToStart -> Alignment.CenterEnd
+                                DismissDirection.StartToEnd -> Alignment.CenterStart
+                            }
+
+                            Box(modifier = Modifier
+                                .fillMaxSize()
+                                .background(color)
+                                .padding(12.dp),
+                                contentAlignment = alignment) {
+                                Icon(icon, contentDescription = "icon", modifier = Modifier.scale(scale))
+                            }
+                        },
+                        dismissContent = {
+                            Card(modifier = Modifier
+                                .fillMaxWidth()
+                                .heightIn(80.dp)
+                                .padding(8.dp)) {
+                            Text(text = item.name)
+                            }
+                        }
+                )
+
+
+
             }
         }
-    }
-    /*Column{
-            Box(
-                modifier = Modifier
-                    .clip(shape = RoundedCornerShape(10.dp))
-                    .padding(8.dp)
-                    .background(Color.Red)
-                    .fillMaxWidth()
-                    .heightIn(80.dp)
-                    .swipeable(
-                        state = swipeableState,
-                        anchors = mapOf(
-                            0f to 0,
-                            -dpToPX(context = context, dpValue = 100f) to 1,
-                            dpToPX(context = context, dpValue = 100f) to 2
-                        ),
-                        thresholds = { _, _ -> FractionalThreshold(0.3f) },
-                        orientation = Orientation.Horizontal
-                    )
-            ) {
-                Button(onClick = { *//*TODO*//* }) {
+
+
+        /*LazyColumn{
+            items(items = itemsList){
+                item ->
+                Box(
+                    modifier = Modifier
+                        .clip(shape = RoundedCornerShape(10.dp))
+                        .padding(8.dp)
+                        //.background(Color.Red)
+                        .fillMaxWidth()
+                        .heightIn(80.dp)
+                        .swipeable(
+                            state = swipeableState,
+                            anchors = mapOf(
+                                0f to 0,
+                                -dpToPX(context = context, dpValue = 100f) to 1,
+                                dpToPX(context = context, dpValue = 100f) to 2
+                            ),
+                            thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                            orientation = Orientation.Horizontal
+                        )
+                ) {
+                    Button(onClick = { *//*TODO*//* }) {
                     Text(text = "btn")
                 }
                 Box(modifier = Modifier
@@ -149,38 +235,16 @@ fun ListScreen(navController: NavHostController, context: Context) {
                     .fillMaxWidth()
                     .heightIn(80.dp)
                     .background(Color.Green)
-                )
+                ){
+                    //Text(text = item.toString())
+                    Text(text = item.name)
+                    //Text(text = "mytext$it")
+                }
             }
-        }*/
-
+        }
+    }*/
 
     }
-
-    /*Box(
-            modifier = Modifier
-                .background(Color.Red)
-                .fillMaxWidth()
-                .heightIn(80.dp)
-                .swipeable(
-                    state = swipeableState,
-                    anchors = mapOf(
-                        0f to 0,
-                        -dpToPX(context = context, dpValue = 100f) to 1,
-                        dpToPX(context = context, dpValue = 100f) to 2
-                    ),
-                    thresholds = { _, _ -> FractionalThreshold(0.3f) },
-                    orientation = Orientation.Horizontal
-                )
-    ) {
-        Box(modifier = Modifier
-            .offset {
-                IntOffset(swipeableState.offset.value.roundToInt(), 0)
-            }
-            .fillMaxWidth()
-            .heightIn(80.dp)
-            .background(Color.Green)
-        )
-    }*/
 
 }
 
